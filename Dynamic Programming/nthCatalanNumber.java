72f771a4761f3a4248d5b8f2b4daf83dc3a09247
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2021-12-01 21:35:21 +0530

    Create nthCatalanNumber.java

---
+++ Dynamic Programming/nthCatalanNumber.java
@@ -0,0 +1,25 @@
+class Solution
+{
+    public BigInteger findCatalan(int n)
+    {
+        return nthCatalan(n,new HashMap<Integer, BigInteger>());
+    }
+    public BigInteger nthCatalan(int n, HashMap<Integer, BigInteger> memo){
+        if(n <= 1){
+            return BigInteger.ONE;
+        }
+        
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+        
+        BigInteger num = BigInteger.ZERO;
+        
+        for(int i = 0; i < n; i++){
+            num = num.add(nthCatalan(i, memo).multiply(nthCatalan(n -i -1, memo)));
+        }
+        
+        memo.put(n, num);
+        return memo.get(n);
+    }
+}
