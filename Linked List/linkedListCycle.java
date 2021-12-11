ed9f032ca0e35a0c07bb3ee0c00c9fc8d99a4885
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2021-12-11 23:22:50 +0530

    Update linkedListCycle.java

--- Linked List/linkedListCycle.java
+++ Linked List/linkedListCycle.java
@@ -26,3 +26,42 @@ public class Solution {
         
     }
 }
+
+
+// O(1) Approach
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+        
+//         if(head==null){
+//             return false;
+//         }
+        
+//         ListNode slowPtr = head;
+        
+//         ListNode fastPtr = head;
+        
+//         while(fastPtr.next != null && fastPtr.next.next != null){
+            
+//             slowPtr = slowPtr.next;
+//             fastPtr = fastPtr.next.next;
+            
+//             if(slowPtr == fastPtr){
+//                 return true;
+//             }
+//         }
+        
+//         return false;
+        
+//     }
+// }
