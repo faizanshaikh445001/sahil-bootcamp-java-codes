5b6fa543a35a212437fbfd1d859d0976b9c9141f
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2021-12-13 22:55:49 +0530

    Update removeNthNodeFromEndOfList.java

--- Linked List/removeNthNodeFromEndOfList.java
+++ Linked List/removeNthNodeFromEndOfList.java
@@ -44,3 +44,38 @@ class Solution {
     }
     
 }
+
+
+
+
+
+// Two Pointer Approach
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+     
+        if(head.next==null || head.next == null)
+            return null;
+        
+        ListNode runner = head;
+        ListNode follower = head;
+        
+        for(int i=0; i<n; i++){
+            runner = runner.next;
+        }
+        
+        if(runner == null){
+            return head.next;
+        }
+        
+        while(runner.next != null){
+            runner = runner.next;
+            follower = follower.next;
+        }
+        
+        follower.next = follower.next.next;
+    
+        return head;
+        
+    }
+    
+}
