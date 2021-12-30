e1db828a6a042354a79de1e0aa2fddcfbba464be
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2021-12-30 11:49:55 +0530

    Update Count number of substrings.java

--- HashMaps/Count number of substrings.java
+++ HashMaps/Count number of substrings.java
@@ -41,3 +41,49 @@ class Solution
   }
     
 }
+
+
+
+// HashMaps Approach Was Giving TLE on GFG
+class Solution
+{
+    long substrCount (String s, int k) {
+        return countAtMostKSubs(s, k) - countAtMostKSubs(s, k - 1);
+    }
+    
+    public static long countAtMostKSubs(String str, int k) {
+    long answer = 0;
+    int release = 0;
+
+    HashMap<Character, Integer> memo = new HashMap<>();
+
+    for (int acquire = 0; acquire < str.length(); acquire++) {
+      char currentChar = str.charAt(acquire);
+
+      if(memo.containsKey(currentChar)){
+          memo.put(currentChar, memo.get(currentChar) + 1);
+      }
+      else{
+          memo.put(currentChar, 1);
+      }
+      
+      while(release <= acquire && memo.size() > k){
+          char disChar = str.charAt(release);
+          memo.put(disChar, memo.get(disChar) - 1);
+          release++;
+          
+          if (memo.get(disChar) == 0) {
+            memo.remove(disChar);
+          }
+          
+      }
+    
+      answer += acquire - release + 1;
+
+    }
+
+    return answer;
+  }
+    
+}
+
